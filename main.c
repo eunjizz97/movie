22537cc8172fdeb3d4228b1407e0d03115bb3c59
eunjizz97 <42995763+eunjizz97@users.noreply.github.com>
2018-11-29 01:32:34 +0900

    11/29

---
+++ main.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "movie.h"
+#include "linkedList.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	FILE *fp = NULL; 		//영화 데이터를 읽어오기 위한 파일 포인터 
+	char name[200];			//영화 제목 변수 
+	char country[10]; 		//영화 제작국가 변수 
+	int runTime; 			//영화 런타임 변수 
+	float score; 			//영화 평점 변수 
+	
+	char c; 
+	
+	int exit_flag = 0; 		//flag variable for while loop
+	int option; 			//사용자가 메뉴에서 선택하는 숫자 
+	void *list, *mvInfo; 	//pointers for linked list and a specific structure instance for a movie data
+	void *ndPtr;			//void pointer for linked list node
+	
+	
+	//1. reading the movie.dat-----------------------------
+	//1.1 FILE open
+	fp = fopen("C:\\Code\\project2\\movie\\movie.dat", "r");	//파일을 읽기모드로 연다. 
+	
+	//1.2 list generation (use function list_genList() )
+	list = list_genList();										//영화 리스트를 만든다. 
+	
+	//1.3 read each movie data from the file and add it to the linked list
+	while ((c=fgetc(fp) !=EOF /* read name, country, runtime and score*/ ))		//
+	{	
+		//generate a movie info instance(mvInfo) with function mv_genMvInfo()
+		fgets();
+		
+		mv_genMvInfo(name, score, runTime, country);			//영화 정보들을 만든다 
+		 
+		list_addTail(mvInfo, list);
+	}
+
+	//1.4 FILE close
+	fclose(fp);				//파일을 닫아준다. 
+	
+	
+	//2. program start
+	while(exit_flag == 0) 
+	{
+		//2.1 print menu message and get input option
+		printf("------------------------- Menu -------------------------\n");
+		printf("1. print all the movies\n");
+		printf("2. search for specific country movies\n");
+		printf("3. search for specific runtime movies\n");
+		printf("4. search for specific score movies\n");
+		printf("5. exit\n");
+		printf("--------------------------------------------------------\n\n");
+		
+		printf("-- select an option : ");
+		scanf("%d", option);
+		
+		switch(option)
+		{
+			case 1: //print all the movies
+				printf("printing all the movies in the list.....\n\n\n");
+				
+				ndPtr = list;
+				while (/* repeat until the ndPtr points to the end node */)
+				{
+					//2.2 print a movie data : use functions of movie.c and linkedList.c
+					//ndPtr = the next node of the ndPtr;
+					//get object of ndPtr to mvInfo void pointer
+					//print the contents of the mvInfo
+				}
+				
+				break;
+				
+			case 2: //print movies of specific country
+				//2.3.1 get country name to search for
+				printf("select a country : ");
+				scanf("%s", contry);
+				
+				ndPtr = list;
+					while (/* repeat until the ndPtr points to the end node */)
+				{
+					//2.3.2 print a movie data : use functions of movie.c and linkedList.c
+					//ndPtr = the next node of the ndPtr;
+					//get object of ndPtr to mvInfo void pointer
+					//if the input country matches to the country of the movie,
+					//then print the contents of the mvInfo
+				}
+				
+				break;
+				
+			case 3:
+				//2.4.1 get minimal runtime value to search for
+				printf("lowest runtime : ");
+				scanf("%d", runTime);
+				
+				ndPtr = list;
+					while (/* repeat until the ndPtr points to the end node */)
+				{
+					//2.4.2 print a movie data : use functions of movie.c and linkedList.c
+					//ndPtr = the next node of the ndPtr;
+					//get object of ndPtr to mvInfo void pointer
+					//if the input runtime is lower than the runtime of the movie,
+					//then print the contents of the mvInfo
+				}
+				
+				break;
+				
+			case 4:
+				//2.5.1 get minimal score value to search for
+				printf("lowest score : ");
+				scanf("%f", score);
+				
+				ndPtr = list;
+					while (/* repeat until the ndPtr points to the end node */)
+				{
+					//2.5.2 print a movie data : use functions of movie.c and linkedList.c
+					//ndPtr = the next node of the ndPtr;
+					//get object of ndPtr to mvInfo void pointer
+					//if the input score is lower than the score of the movie,
+					//then print the contents of the mvInfo
+				}
+				break; 
+				
+			case 5:
+				printf("Bye!\n\n");
+				exit_flag = 1;
+				break;
+				
+			default:
+				printf("wrong command! input again\n");
+				break;
+		}
+	}
+
+	return 0;
+}
---
+++ movie.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "movie.h"
+
+typedef struct movInfo{	//name, score, runTime, madeIn 의 정보를 가지는 구조체 'movInfo' 를 만듦 
+	char name[200];		//영화의 이름 
+	float score;		//영화의 평점 
+	int runTime;		//영화의 런타임 
+	char madeIn[10];	//영화의 제작국가 
+} movInfo_t;			//구조체 'movInfo' 를 'movInfo_t' 를 통해 정의함 
+
+
+void* mv_genMvInfo(char* name, float score, int runTime, char* country)
+{
+	movInfo_t* mvPtr;
+	
+	//allocate memory and set the member variables
+	
+	return (void*)mvPtr;
+}
+
+void mv_print(void* obj)
+{
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	
+	if (mvPtr == NULL)
+	{
+		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
+	}
+	
+	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
+	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+	
+	return;
+}
+
+
+//return the score value from the input instance of movInfo_t structure
+float mv_getScore(void* obj)
+{
+	
+}
+
+//return the runtime value from the input instance of movInfo_t structure
+int mv_getRunTime(void* obj)
+{
+	
+}
+
+//return the name string pointer from the input instance of movInfo_t structure
+char* mv_getName(void* obj)
+{
+	
+}
+
+//return the country string pointer from the input instance of movInfo_t structure
+char* mv_getCountry(void* obj)
+{
+	
+}
+
+
